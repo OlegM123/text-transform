dac558f9279a30f889b736119b1947c458acf590
Oleg_Muhachev <muhachev.fortech@gmail.com>
2023-01-30 13:37:22 +0300

    code optimisation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function App() {
+  
+  const msgLimit = 140;
   const [text, setText] = useState('');
   const [result, setResult] = useState([]);
   const [workText, setWorkText] = useState([]);
@@ -12,34 +14,32 @@ function App() {
 
   const handleClick = () => {
 
-    if (text.length > 140) { //if text lenght less or equals 140 we just push it as a result
+    if (text.length > msgLimit) { //if text lenght less or equals 140 we just push it as a result
 
-      const countOfFragments = Math.ceil((text.length + ((Math.ceil(text.length / 140).toString().length * 2) + 3) * Math.ceil(text.length / 140)) / 140)
+      const countOfFragments = Math.ceil((text.length + ((Math.ceil(text.length / msgLimit).toString().length * 2) + 3) * Math.ceil(text.length / msgLimit)) / msgLimit)
       const resArr = []; //result
       let i = workText[0].length; // length of current fragment counter
-      let j = 0; // offset
-      let a = 1; // fragments counter
+      let offset = 0; // offset
+      let fragmentCounter = 1; // fragments counter
 
-      while (a <= countOfFragments) {
+      while (fragmentCounter <= countOfFragments) {
 
         let item = '';
 
         while (
-
           i < 140 &&
-          workText[j] !== undefined && // that means end of initial array
-          (item.length + workText[j].length + countOfFragments.toString().length + a.toString().length + 2) <= 140 // that checks if result item length will less than 140 after adding suffix 
-
+          workText[offset] !== undefined && // that means end of initial array
+          (item.length + workText[offset].length + countOfFragments.toString().length + fragmentCounter.toString().length + 2) <= 140 // that checks if result item length will less than 140 after adding suffix 
         ) {
           // ^ sum of length of current fragment, current word that we want to add, number of current dragment, number of count of fragments and symbols " /" 
-          item += workText[j] + ' ';
+          item += workText[offset] + ' ';
           i = item.length;
-          j++;
+          offset++;
         }
 
         i = 0;
-        resArr.push(item + a + '/' + countOfFragments);
-        a++;
+        resArr.push(item + fragmentCounter + '/' + countOfFragments);
+        fragmentCounter++;
 
       }
       setResult(resArr);

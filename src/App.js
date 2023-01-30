a426a370242457413d1b572c393baf2aedfbd101
Oleg_Muhachev <muhachev.fortech@gmail.com>
2023-01-30 12:19:16 +0300

    logic fixes and comments added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function App() {
@@ -6,27 +6,47 @@ function App() {
   const [result, setResult] = useState([]);
   const [workText, setWorkText] = useState([]);
 
+  useEffect(() => {
+    setWorkText(() => text.split(' ')); // text splitted by ' ' symbol   
+  }, [text])
+
   const handleClick = () => {
-    const countOfFragments = Math.ceil(text.length / 140);
-    const resArr = [];
-    setWorkText(() => text.split(' '));
-    let i = workText[0].length;
-    let j = 0;
-    let a = 1;
-    while (a <= countOfFragments) {
-      let item = '';
-      while (i < 140) {
-        if (workText[j] === undefined) break;
-        if (item.length + workText[j].length > 140) break;
-        item += workText[j] + ' ';
-        i = item.length;
-        j++;
+
+    if (text.length > 140) { //if text lenght less or equals 140 we just push it as a result
+
+      const countOfFragments = Math.ceil((text.length + ((Math.ceil(text.length / 140).toString().length * 2) + 3) * Math.ceil(text.length / 140)) / 140)
+      const resArr = []; //result
+      let i = workText[0].length; // length of current fragment counter
+      let j = 0; // offset
+      let a = 1; // fragments counter
+
+      while (a <= countOfFragments) {
+
+        let item = '';
+
+        while (
+
+          i < 140 &&
+          workText[j] !== undefined && // that means end of initial array
+          (item.length + workText[j].length + countOfFragments.toString().length + a.toString().length + 2) <= 140 // that checks if result item length will less than 140 after adding suffix 
+
+        ) {
+          // ^ sum of length of current fragment, current word that we want to add, number of current dragment, number of count of fragments and symbols " /" 
+          item += workText[j] + ' ';
+          i = item.length;
+          j++;
+        }
+
+        i = 0;
+        resArr.push(item + a + '/' + countOfFragments);
+        a++;
+
       }
-      i = 0;
-      resArr.push(item + a + '/' + countOfFragments)
-      a++;
+      setResult(resArr);
+    } else {
+      setResult([text]);
     }
-    setResult(resArr);
+
   }
 
   return (
@@ -34,14 +54,13 @@ function App() {
       input text:
       <StyledInput onChange={(e) => setText(e.target.value)}>
       </StyledInput>
-      <StyledButton onClick={() => handleClick()}>
+      <StyledButton onClick={handleClick}>
         run
       </StyledButton>
       <br />
-      <br />
-      result: {result.map(item => {
+      result: {result.map((item, index) => {
         return (
-          <ResultItem>
+          <ResultItem key={index}>
             {item}
           </ResultItem>
         )
